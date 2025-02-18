67cc9eed079282e884644e21a9b474a8eee277d5
MatheusCosta616 <matheus.costa2616@gmail.com>
2025-02-17 22:07:15 -0300

    comitado

---
+++ study-apix/src/main/java/com/github/matheuscosta616/study_apix/StudyApixApplication.java
@@ -0,0 +1,13 @@
+package com.github.matheuscosta616.study_apix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApixApplication.class, args);
+	}
+
+}
---
+++ study-apix/src/test/java/com/github/matheuscosta616/study_apix/StudyApixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.matheuscosta616.study_apix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
